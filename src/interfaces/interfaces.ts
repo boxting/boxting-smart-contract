85263fe0cb995e3e7b555410251039af839b6236
RodrigoAGM <rod-gm@hotmail.com>
2021-02-23 02:18:22 -0500

    Create interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,37 @@
+export interface CandidateData {
+    id: string
+    electionId: string
+    firstName: string
+    lastName: string
+    imageUrl: string
+}
+
+export interface EventData {
+    id: string
+    startDate: string
+    endDate: string
+}
+
+export interface ElectionData {
+    id: string
+    eventId: string
+    electionType: 'single' | 'multiple'
+    maxVotes: number
+}
+
+export interface InitData {
+    event: EventData
+    elections: ElectionData[]
+    candidates: CandidateData[]
+}
+
+export interface VoterData {
+    id: string
+    firstName: string
+    lastName: string
+}
+
+export interface JsonResponse {
+    key?: string
+    record?: any
+}
